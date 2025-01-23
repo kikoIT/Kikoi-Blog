e0b74d8cd92e9fc9c577e4b894f38c5c3d211c09
Barake <kikoisoftwares@gmail.com>
2025-01-23 07:48:53 +0300

    updated logout logic controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -79,7 +79,7 @@ const login_post = async (req, res) => {
 }
 
 const logout_get = (req, res) => {
-    res.cookie('jwt', '', { maxAge: 1 });
+    res.clearCookie("jwt");
     res.redirect('/');
 }
 
